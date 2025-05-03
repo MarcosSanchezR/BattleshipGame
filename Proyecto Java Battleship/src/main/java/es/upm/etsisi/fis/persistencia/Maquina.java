ac20e0024360e4a5ae7db6e30a2ec4695ca65e3a
Daniel Rivera <daniel.rivera@alumnos.upm.es>
2025-05-03 19:59:31 +0200

    Plantilla Maquina

---
+++ Proyecto Java Battleship/src/main/java/es/upm/etsisi/fis/persistencia/Maquina.java
@@ -0,0 +1,15 @@
+package es.upm.etsisi.fis.persistencia;
+
+public class Maquina {
+
+    private final String id;
+
+    public Maquina(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+}
