145317fe11ba14a84482ebce07a139c97d4b452f
Daniel Rivera <daniel.rivera@alumnos.upm.es>
2025-05-25 11:57:55 +0200

    Últimas correcciones en los barcos

--- Proyecto Java Battleship/src/main/java/es/upm/etsisi/fis/state/Portaviones.java
+++ Proyecto Java Battleship/src/main/java/es/upm/etsisi/fis/state/Portaviones.java
@@ -18,12 +18,11 @@ public class Portaviones extends Barco {
     }
 
     //@TODO: Implementar RF #18745
-    private Ataque habilidadPortaaviones() {
+    private void habilidadPortaaviones() {
         Jugador jugador = super.getTablero().getPropietario();
         Tablero tableroObjetivo = GameManager.getInstance().getTableroRival(jugador);
         Ataque ataqueRealizado = GameManager.getInstance().realizarAtaqueReglamentario(jugador, tableroObjetivo);
         jugador.aniadirAtaque(ataqueRealizado);
-        return ataqueRealizado;
     }
 
 }
