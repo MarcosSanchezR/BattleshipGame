0514793f2b55c0d010e6a96e84b25e36978e725d
Darío <dario.dominguez@almnos.upm.es>
2025-05-25 18:03:17 +0200

    optimizados for each

--- Proyecto Java Battleship/src/main/java/es/upm/etsisi/fis/state/Tablero.java
+++ Proyecto Java Battleship/src/main/java/es/upm/etsisi/fis/state/Tablero.java
@@ -81,14 +81,16 @@ public class Tablero {
                 Casilla casilla = casillas[i][j];
                 boolean ocupado = false;
 
-                // Comprobar si la casilla está ocupada por algún barco enemigo
-                for (Barco barco : barcosPropios) { // Aquí barcosPropios serían los barcos del rival en este tablero
+                int x = 0;
+                while (x < barcosPropios.size() && !ocupado) {
+                    Barco barco = barcosPropios.get(x);
                     if (barco.getCasillasOcupadas().contains(casilla)) {
                         ocupado = true;
-                        //@FIXME: Corregir este for anidado con un while/do-while
                     }
+                    x++;
                 }
-            if(casilla.isRevelada()){
+
+                if(casilla.isRevelada()){
                 if(casilla.getBarco().isPresent()){
                 System.out.print(" 🚢 "); // Barco revelado
             }else {
@@ -116,12 +118,13 @@ public class Tablero {
                 Casilla casilla = casillas[i][j];
                 boolean ocupado = false;
 
-                // Recorremos los barcos para ver si la casilla está ocupada
-                for (Barco barco : barcosPropios) {
+                int k = 0;
+                while (k < barcosPropios.size() && !ocupado) {
+                    Barco barco = barcosPropios.get(k);
                     if (barco.getCasillasOcupadas().contains(casilla)) {
                         ocupado = true;
-
                     }
+                    k++;
                 }
 
                 if (ocupado) {
